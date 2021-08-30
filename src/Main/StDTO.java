61353fbccf4c3c060c96adb51af0a9a9dc8e21cc
HanGilyong <shkg13@naver.com>
2021-08-30 18:08:33 +0900

    최초 학생관리프로젝트

---
+++ src/Main/StDTO.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class StDTO {
+	private String stNum;
+	private String name;
+	private String phoneNum;
+	private String age;
+	public String getStNum() { return stNum; }
+	public void setStNum(String stNum) { this.stNum = stNum; }
+	public String getName() { return name; }
+	public String getPhoneNum() { return phoneNum; }
+	public void setPhoneNum(String phoneNum) { this.phoneNum = phoneNum; }
+	public String getAge() { return age; }
+	public void setAge(String age) { this.age = age; }
+	public void setName(String name) { this.name = name; }
+}
---
+++ src/Main/main.java
@@ -0,0 +1,23 @@
+package Main;
+
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num ;
+		String name, stNum, phoneNum, age;
+		while(true) {
+			System.out.println("학생 관리 프로그램");
+			System.out.println("1. 한길용 2. 이진원 3. 진동수 4. 이창훈 5. 이동호");
+			num = sc.nextInt();
+			switch(num) {
+			case 1 : break;
+			case 2 : break;
+			case 3 : break;
+			case 4 : break;
+			case 5 : break;
+			}
+		}
+	}
+}
