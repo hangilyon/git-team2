6e3f763cef0fd86bc786ec29c21cab51a507c1fb
HanGilyong <shkg13@naver.com>
2021-08-30 21:43:41 +0900

    han 수정자료

---
+++ src/Han/StDTO.java
@@ -0,0 +1,21 @@
+package Han;
+
+
+
+public class StDTO {
+	private String stNum;
+	private String name;
+	private String phoneNum;
+	private String age;
+	private boolean bool;
+	public boolean isBool() { return bool;}
+	public void setBool(boolean bool) {this.bool = bool;}
+	public String getStNum() { return stNum; }
+	public void setStNum(String stNum) { this.stNum = stNum; }
+	public String getName() { return name; }
+	public String getPhoneNum() { return phoneNum; }
+	public void setPhoneNum(String phoneNum) { this.phoneNum = phoneNum; }
+	public String getAge() { return age; }
+	public void setAge(String age) { this.age = age; }
+	public void setName(String name) { this.name = name; }
+}
--- src/Main/main.java
+++ src/Main/main.java
@@ -2,6 +2,8 @@ package Main;
 
 import java.util.Scanner;
 
+import Han.han;
+
 public class main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -12,7 +14,10 @@ public class main {
 			System.out.println("1. 한길용 2. 이진원 3. 진동수 4. 이창훈 5. 이동호");
 			num = sc.nextInt();
 			switch(num) {
-			case 1 : break;
+			case 1 : 
+				han han = new han();
+				han.display();
+				break;
 			case 2 : break;
 			case 3 : break;
 			case 4 : break;
